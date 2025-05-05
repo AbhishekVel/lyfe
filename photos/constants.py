d30b9ee77232b255b234dd3b601b69f46efb943c
Abhishek Velayudham <velayudham.abhishek@gmail.com>
2025-05-04 22:26:04 -0400

    upload captions, use uv

---
+++ photos/constants.py
@@ -0,0 +1,11 @@
+"""Constants used across the photos service."""
+
+# Pinecone index name for storing photo embeddings
+PHOTOS_INDEX_NAME = "majestic-eucalyptus"
+
+# Namespace for personal photos in Pinecone
+PHOTOS_NAMESPACE = "abhivel-photos"
+LOCATION_NAMESPACE = "abhivel-location"
+
+
+VECTOR_DIMENSION = 512
